bb696e8434d988c560281df10b577730903a5304
timsheng <sly19870601@gmail.com>
2013-05-13 18:18:47 +0800

    add consultation 9 cases

--- app/cases/clientportal/consultation_spec.rb
+++ app/cases/clientportal/consultation_spec.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require File.expand_path 'app/cases/spec_helper'
 require File.expand_path 'app/cases/shared/action'
 
@@ -13,6 +14,7 @@ describe 'consultation page', :consultation=>false do
     # let(:consultation_page){$navi.goto_home_page.direct_to_login_page.login("gzu@capvision.com","1234").direct_to_consultation_page}
     
 		context 'page loading' do
+
 			it 'should visit consultation page' do 
 				# home_page = $navi.goto_home_page
 				# login_page = home_page.direct_to_login_page
@@ -21,17 +23,65 @@ describe 'consultation page', :consultation=>false do
 				consultation_page.my_profile?.should be_true
 			end
 
-			xit 'should be xxx' do 
+		end
+	end
 
+	describe 'consultations module' do 
+
+		context 'verify project list type' do 
+
+			it 'should contains recommended and requested and arranged and completed' do 
+				consultation_page.project_list_type.should match(/^(Recommended)|^(Requested)|^(Arranged)|^(Completed)/)
 			end
 
-			it 'should pending' do
-				pending 
+			it 'should be recommended type by default selected' do 
+				consultation_page.recommended_status_element.attribute(:class).should eql 'active'
 			end
 
-			it 'should pass' do
-			    consultation_page 
+			it 'should be able to select requested type' do 
+				consultation_page.requested_status
+				sleep 2
+				consultation_page.requested_status_element.attribute(:class).should eql 'active'
+			end
+
+			it 'should be able to select arranged type' do 
+				consultation_page.arranged_status
+				sleep 2
+				consultation_page.arranged_status_element.attribute(:class).should eql 'active'
+			end
+
+			it 'should be able to select completed type' do 
+				consultation_page.completed_status
+				sleep 2
+				consultation_page.completed_status_element.attribute(:class).should eql 'active'
+			end
+		end
+
+		context 'verify right corresponding description' do 
+
+			it 'should be right by recommended' do 
+				# consultation_page.recommended_status
+				consultation_page.blank_task_detail.should eql 'All the consultants we’ve recommended to you. Select the consultant you are interested in to see detailed information. Request a call and we will arrange the consultation for you.'
+			end
+
+			it 'should be right by requested' do 
+				consultation_page.requested_status
+				sleep 2
+				consultation_page.blank_task_detail.should eql 'All the consultants you would like to speak to. We are currently working on arranging these consultations for you.'
+			end
+
+			it 'should be right by arranged' do 
+				consultation_page.arranged_status 
+				sleep 2
+				consultation_page.blank_task_detail.should eql 'All the consultants that you have scheduled. Select the consultant to reschedule or cancel the call. Select the consultant and click "called" to confirm the call and give your valuable feedback.'
+			end
+
+			it 'should be right by completed', :test do 
+				consultation_page.completed_status
+				sleep 2
+				consultation_page.blank_task_detail.should eql 'All the consultations you have completed.'
 			end
 		end
 	end
 end
+
--- app/pages/clientportal/consultation_page.rb
+++ app/pages/clientportal/consultation_page.rb
@@ -1,6 +1,19 @@
 class ConsultationPage < ClientportalPage
 	page_url "#{$config.host}consultation/project/index"
 
-	link :my_profile, :text=>'My Profile'
+	link :my_profile, :text => 'My Profile'
 
+	div :project_list_type, :id => 'project-list-type'
+
+	link :recommended_status, :id => 'recommended-status'
+
+	link :requested_status, :id => 'requested-status'
+
+	#link :requested_status_active, :id => 'requested-status',:class => 'active'多属性定位
+
+	link :arranged_status, :id => 'arranged-status'
+
+	link :completed_status, :id => 'completed-status'
+
+	div :blank_task_detail, :id => 'blank-task-detail'
 end
